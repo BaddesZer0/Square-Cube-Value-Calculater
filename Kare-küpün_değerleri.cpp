1816c9ea394518064ccfd17c37f23611dc5cda48
Arda Aras <ardapatu@gmail.com>
2025-11-28 14:04:21 +0300

    Kaynak kod

---
+++ Kare-küpün_değerleri.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int kenar;
+
+	cout << "Degerlerinin hesaplanmasi icin karenizin bir kenarinin uzunlugunu giriniz:";
+	cin >> kenar;
+
+	cout << "Kare ve küp icin degerler asagidaki gibidir:" << endl;
+
+	cout << "Karenin cevresi:" << kenar * 4 << endl;
+
+	cout << "Karenin alani:" << kenar * kenar << endl;
+
+	cout << "Kupun dis yuzey alani:" << 6 * kenar * kenar << endl;
+
+	cout << "Kupun hacmi:" << kenar * kenar * kenar << endl;
+
+	cout << "Kupun cevresi:" << 12 * kenar << endl;
+	
+	return 0;
+}
